a04dfd6b42b895f535b2d58efdcaf5a1068cefc4
Rajvickram <vikrampalani10@gmail.com>
2023-10-16 12:37:42 +0530

    New BusDemo.java file added

--- BusDemo.java
+++ BusDemo.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 class BusDemo {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int userOpt = 1;
+
+        while (userOpt == 1) {
+            System.out.println("Enter 1 to Book and 2 to Exit");
+
+            userOpt = sc.nextInt();
+
+            if (userOpt == 1) {
+                System.out.println("Booking...........");
+            }
+        }
     }
 }
