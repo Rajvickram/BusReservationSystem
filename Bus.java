c480e567cd4c5bae83fc26eb7b26e3a90903a64d
Rajvickram <vikrampalani10@gmail.com>
2023-10-16 12:43:24 +0530

    Modified in Bus.java file

--- Bus.java
+++ Bus.java
@@ -1,5 +1,37 @@
-
+import java.util.*;
 
 class Bus {
-    
+    private int busNo;
+
+    private boolean ac;
+
+    private int capacity;
+
+    public void setBusNo(int busNo) {
+        this.busNo = busNo;
+    }
+
+    public int getBusNo() {
+        return busNo;
+    }
+
+    public void setAC(boolean ac) {
+        this.ac = ac;
+    }
+
+    public boolean getAC() {
+        return ac;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void displayBusInfo() {
+        System.out.println("Bus No : " + busNo + ", Ac Seats : " + ac + ", Capacity Seats Available : " + capacity);
+    }
 }
