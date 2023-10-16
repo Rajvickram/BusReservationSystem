ba6f684ae9c2b07c08cc56f4f4ce4e94af4b2292
Rajvickram <vikrampalani10@gmail.com>
2023-10-16 13:08:09 +0530

    Added new BusDemo.java file will be Modified

--- BusDemo.java
+++ BusDemo.java
@@ -5,6 +5,16 @@ class BusDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        ArrayList<Bus> buses = new ArrayList<Bus>();
+        buses.add(new Bus(1, true, 3));
+        buses.add(new Bus(2, false, 5));
+        buses.add(new Bus(3,true, 2));
+
+
+        for (Bus b : buses) {
+            b.displayBusInfo();
+        }
+
         int userOpt = 1;
 
         while (userOpt == 1) {
